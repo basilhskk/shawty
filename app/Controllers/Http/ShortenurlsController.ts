b9fc2a6ccfe434085be967c9163a32b05822f937
V Koutsokostas <you@example.com>
2021-09-02 14:42:17 +0300

    fix

---
+++ app/Controllers/Http/ShortenurlsController.ts
@@ -0,0 +1,12 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ShortenurlsController {
+
+    async shortenUrlRandom({params}){
+        
+    }
+
+    async shortenUrlAlias({params}){
+
+    }
+}
